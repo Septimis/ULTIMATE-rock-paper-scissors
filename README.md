e3523caaf4e64f97272e9e48dd82ea2c5b7aa2a9
Septimis <connor.meads@protonmail.com>
2021-07-10 15:30:36 -0600

    start to a large improvement.  More to come

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# The Odin Project | Challenge 2: Rock Paper Scissors (Console)
+# The Odin Project | Challenge 2: Rock Paper Scissors (DOM Practice)
 You can find my page here: https://septimis.github.io/rock-paper-sissors/
 
 ## Description
-This is the Odin Projects introduction to Javascript.  The user can play Rock Paper Scissors by interacting with the <i>console</i> in the Developers Tools.  To open devleoper tools, select your browser for a tutorial: [chrome](https://developer.chrome.com/docs/devtools/), [firefox](https://developer.mozilla.org/en-US/docs/Tools), [Edge](https://docs.microsoft.com/en-us/microsoft-edge/devtools-guide-chromium/open/).  Upon loading the page, the user will have a pop up asking how many times they would like to run the game, and then the user must enter in 'rock', 'paper', or 'scissors' for each game.  A final tally will be calculated at the end and the winner will be announced.
+The game is simple, select your weapon: <b>Sturdy Rock, Mystical Paper, or Vicious Scissors.</b><br>
+
+
+## Game Guide
+1. Each round, the player and computer will chose either Rock, Paper, or Scissors.  The first to 5 points wins the game.
+1. Each round will reward you with points **if** you win.
+    - You will recieve 5 * how many points you won the game by
+1. You may use points buy various upgrades and items.
+    
 
 ## Challenges & Learning
 I ran into a problem at the beginning just trying to get my JavaScript file to run.  I found that the link to a JavaScript file needs to be referenced at the bottom of a page, not at the top as any `document.getElementById()` will return with null as the html id's have not been initiated yet.
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//button functionality
+let rockBtn = document.getElementById("rockBtn");
+rockBtn.addEventListener("click", function() {
+    playRound(computerPlay(), "rock");
+});
+
+let paperBtn = document.getElementById("paperBtn");
+paperBtn.addEventListener("click", function() {
+    playRound(computerPlay(), "paper");
+});
+
+let scissorsBtn = document.getElementById("scissorsBtn");
+scissorsBtn.addEventListener("click", function() {
+    playRound(computerPlay(), "scissors");
+});
+
 //randomly returns with 'Rock', 'Paper', or 'Scissors'
 function computerPlay() {
     //declare return variable
@@ -17,33 +33,13 @@ function computerPlay() {
     return l_computerChoice;
 }
 
-function getChoice() {
-    //get User Input
-    let l_badInput = true;
-    let l_playerChoice = "";
-    //loop to verify for correct input
-    do {
-        l_playerChoice = prompt("Rock? Paper? Or Scissors?: ", "Your choice here...").toLowerCase();
-        if(l_playerChoice == null) {
-            alert("You didn't input anything!  Please try again...");
-            l_badInput = true;
-        } else if(l_playerChoice == "rock" || l_playerChoice == "paper" || l_playerChoice == "scissors") {
-            l_badInput = false;
-        } else {
-            console.log("Please enter 'rock', 'paper', or 'scissors'...")
-            l_badInput = true;
-        }
-    } while(l_badInput);
-    return l_playerChoice;
-}
-
 //plays one round of rock, paper, scissors
 function playRound(a_computerChoice, a_playerChoice) {
     //declare variable that  shows the winner --> 0 means draw, 1 means player point, 2 means computer point
     let winner = 0;
 
     //show who got what
-    console.log(`The computer chose ${a_computerChoice} & you chose ${a_playerChoice}`);
+    document.getElementById("results").innerText = `The computer chose ${a_computerChoice} & you chose ${a_playerChoice}`;
 
     //logic of the game
     if(a_computerChoice == a_playerChoice) {
@@ -70,44 +66,3 @@ function playRound(a_computerChoice, a_playerChoice) {
     }
     return winner;
 }
-
-
-function game() {
-    //declare local variables
-    let l_badInput = false;
-    let l_numGames = 5;
-    let l_playerScore = 0;
-    let l_computerScore = 0;
-
-    //loop to catch bad input
-    do {
-       try {
-            l_numGames = parseInt(prompt("Enter the number of times you would like to play"));
-            l_badInput = false;
-        } catch(e) {
-            alert("Incorrect input! Enter an integer and try again...");
-            l_badInput = true;
-        } 
-    } while(l_badInput);
-    
-    //play numGames number of times
-    for(let i = 0; i < l_numGames; i++) {
-        let l_score = playRound(computerPlay(), getChoice());
-        if(l_score == 1) {
-            l_playerScore++;
-        } else if(l_score == 2) {
-            l_computerScore++;
-        }
-    }
-
-    //announce winner
-    console.log(`The score was:\nPlayer: ${l_playerScore}\nComputer: ${l_computerScore}`);
-    if(l_playerScore == l_computerScore) {
-        console.log("It was a DRAW!");
-    } else if(l_playerScore > l_computerScore) {
-        console.log("You won!")
-    } else{
-        console.log("The computer won!");
-    }
-}
-//game();
