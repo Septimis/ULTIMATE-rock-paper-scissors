7db2f8b2da2a5f494269e91f2ba1f1c8fcf28a7d
Connor Meads <37027660+Septimis@users.noreply.github.com>
2021-07-13 22:31:13 +0000

    Added a game page & floating guide page.

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
-//HOMESCREEN
-//floatDown animation items
-let startGameBtn = document.getElementById("startButton");
-let userStats = document.getElementById("userStats");
+//Initial VARIABLES
+let l_body = document.getElementById("body");
 
+//HOMESCREEN
+let l_homeScreenItems = document.querySelectorAll(".homeScreen");
+let l_homeScreenClone = l_homeScreenItems;
+let l_startGameBtn = document.getElementById("startButton");
+
+//floatDown animation items
+let l_floatItems = document.querySelectorAll(".animatedItems");
 
 //upgrade buttons
-let rockUpgradeBtn = document.getElementById("rockUpgradeBtn");
-let paperUpgradeBtn = document.getElementById("paperUpgradeBtn");
-let scissorUpgradeBtn = document.getElementById("scissorUpgradeBtn");
-let expBonusBtn = document.getElementById("expBonusBtn");
+let l_rockUpgradeBtn = document.getElementById("rockUpgradeBtn");
+let l_paperUpgradeBtn = document.getElementById("paperUpgradeBtn");
+let l_scissorUpgradeBtn = document.getElementById("scissorUpgradeBtn");
+let l_expBonusBtn = document.getElementById("expBonusBtn");
 
-//button functionality
-startGameBtn.addEventListener("click", function() {
-    startGameBtn.
-    userStats.style.animationPlayState = "initial";
+//GAMESCREEN
+let l_gameScreenItems = document.querySelectorAll(".gameScreen");
+let l_returnBtn = document.getElementById("backBtnContainer");
+
+//Defaults
+//gameScreen elements are hidden by default
+for(let i = 0; i < l_gameScreenItems.length; i++) {
+  l_gameScreenItems[i].remove();
+}
+
+//BUTTON FUNCTIONALITY
+//homeScreen
+l_startGameBtn.addEventListener("click", function(e) {
+  l_homeScreenClone = l_homeScreenItems;
+  for(let i = 0; i < l_homeScreenItems.length; i++) {
+    //remove homeScreen items
+    l_homeScreenItems[i].remove();
+
+    //add gameScreen items
+    l_body.appendChild(l_gameScreenItems[i]);
+
+  }
 });
 
 //GAMESCREEN
 //button functionality
+l_returnBtn.addEventListener("click", function(e) {
+  for(let i = 0; i < l_gameScreenItems.length; i++) {
+    //remove gameScreen items
+    l_gameScreenItems[i].remove();
 
+    //add homeScreen items
+    l_body.appendChild(l_homeScreenItems[i]);
+  }
+});
 
 //GAME LOGIC
 //randomly returns with 'rock', 'paper', or 'scissors'
@@ -39,3 +70,7 @@ function getComputerChoice() {
     }
     return l_computerChoice;
 }
+
+/* Reset Animations
+ * https://css-tricks.com/restart-css-animation/
+ */
