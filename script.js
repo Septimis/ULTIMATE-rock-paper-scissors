68e50f0ca88044606e7fd89c6c26301ccc9e1c5a
Septimis <connor.meads@protonmail.com>
2021-07-12 20:54:24 -0600

    added animations

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+//HOMESCREEN
+//floatDown animation items
+let startGameBtn = document.getElementById("startButton");
+let userStats = document.getElementById("userStats");
+
+
+//upgrade buttons
+let rockUpgradeBtn = document.getElementById("rockUpgradeBtn");
+let paperUpgradeBtn = document.getElementById("paperUpgradeBtn");
+let scissorUpgradeBtn = document.getElementById("scissorUpgradeBtn");
+let expBonusBtn = document.getElementById("expBonusBtn");
+
 //button functionality
-let rockBtn = document.getElementById("rockBtn");
-rockBtn.addEventListener("click", function() {
-    playRound(computerPlay(), "rock");
+startGameBtn.addEventListener("click", function() {
+    startGameBtn.
+    userStats.style.animationPlayState = "initial";
 });
 
-let paperBtn = document.getElementById("paperBtn");
-paperBtn.addEventListener("click", function() {
-    playRound(computerPlay(), "paper");
-});
+//GAMESCREEN
+//button functionality
 
-let scissorsBtn = document.getElementById("scissorsBtn");
-scissorsBtn.addEventListener("click", function() {
-    playRound(computerPlay(), "scissors");
-});
 
-//randomly returns with 'Rock', 'Paper', or 'Scissors'
-function computerPlay() {
+//GAME LOGIC
+//randomly returns with 'rock', 'paper', or 'scissors'
+function getComputerChoice() {
     //declare return variable
     let l_computerChoice = "";
 
@@ -31,38 +38,4 @@ function computerPlay() {
         l_computerChoice = "scissors";
     }
     return l_computerChoice;
-}
-
-//plays one round of rock, paper, scissors
-function playRound(a_computerChoice, a_playerChoice) {
-    //declare variable that  shows the winner --> 0 means draw, 1 means player point, 2 means computer point
-    let winner = 0;
-
-    //show who got what
-    document.getElementById("results").innerText = `The computer chose ${a_computerChoice} & you chose ${a_playerChoice}`;
-
-    //logic of the game
-    if(a_computerChoice == a_playerChoice) {
-        console.log(`Draw!  You both chose ${a_computerChoice}`);
-        winner = 0;
-    } else if(a_computerChoice == "rock" && a_playerChoice == "scissors") {
-        winner = 2;
-        console.log("The computer won!  Rock beats scissors!");
-    } else if(a_computerChoice == "paper" && a_playerChoice == "rock") {
-        winner = 2;
-        console.log("The computer won!  Paper beats rock!");
-    } else if(a_computerChoice == "scissors" && a_playerChoice == "paper") {
-        winner = 2;
-        console.log("The computer won!  Scissors beats paper!");
-    } else if(a_playerChoice == "rock" && a_computerChoice == "scissors") {
-        winner = 1;
-        console.log("You won!  Rock beats scissors!");
-    } else if(a_playerChoice == "paper" && a_computerChoice == "rock") {
-        winner = 1;
-        console.log("You won! Paper beats rock!");
-    } else if(a_playerChoice == "scissors" && a_computerChoice == "paper") {
-        winner = 1;
-        console.log("You won! Scissors beats paper!");
-    }
-    return winner;
 }
